1c6b9b2a2426f3f8ff11445ca319449c19cc5b15
abdullah <abdullahbinmasood02@gmail.com>
2026-05-17 16:03:48 +0500

    added uncaught exception and unhandled rejection handling

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ dotenv.config({ path: "./config.env" });
 const DB = process.env.DB.replace("<PASSWORD>", process.env.PASSWORD);
 const PORT = process.env.PORT || 3000;
 console.log(process.env.NODE_ENV);
+
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, "\n", err.message);
+  server.close(() => process.exit(1));
+});
+
+process.on("uncaughtException", (err) => {
+  console.log("uncaught exception", "\n", err.name, "\n", err.message);
+  process.exit(1);
+});
+
 mongoose.connect(DB).then(() => console.log("DB CONNECTION SUCCESSFUL"));
 
 const server = app.listen(PORT, () => `server is listening at port ${PORT}`);
